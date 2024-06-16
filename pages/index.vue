ba3ca69a06290980551e01753c477e5ba873aee8
Douglas Lopes <dl.aguiar@yahoo.com.br>
2024-06-16 19:05:36 -0300

    define layout base

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Activity, ArrowUpRight, CircleUser, CreditCard, DollarSign, Menu, Package2, Search, Users } from 'lucide-vue-next'
+import { Badge } from '@/components/ui/badge'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+</script>
+
+<template>
+  <main class="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+    <div class="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+      <Card>
+        <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle class="text-sm font-medium">
+            Total Revenue
+          </CardTitle>
+          <DollarSign class="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
+        <CardContent>
+          <div class="text-2xl font-bold">
+            $45,231.89
+          </div>
+          <p class="text-xs text-muted-foreground">
+            +20.1% from last month
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  </main>
+</template>
